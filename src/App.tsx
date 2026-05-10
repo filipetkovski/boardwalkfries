143c5f643c3bfe20ebc6cdcb9ced3fac13bc187c
Filip Petkovski <filip.petkovski.work@gmail.com>
2026-05-10 13:10:01 +0200

    Add margin to the menu popup

--- src/App.tsx
+++ src/App.tsx
@@ -270,7 +270,7 @@ export default function App() {
 
       {/* Menu Modal */}
       {isMenuModalOpen && (
-        <div className="fixed inset-0 z-[100] flex items-center justify-center pt-[20px] pb-[60px] px-2 md:p-8">
+        <div className="fixed inset-0 z-[100] flex items-center justify-center pt-[20px] pb-[20px] px-2 md:p-8">
           <motion.div 
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
